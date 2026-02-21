2a069a259ef2b1b351f174ac0406c29e594e10c5
Jayant <148239272+jay15git@users.noreply.github.com>
2026-02-21 20:01:29 +0530

    implement vapi voice sdk

--- components/CTA.tsx
+++ components/CTA.tsx
@@ -9,13 +9,11 @@ const Cta = () => {
                 Build and Personalize Learning Companion
             </h2>
             <p>Pick a name, subject, voice, & personality — and start learning through voice conversations that feel natural and fun.</p>
-            <Image src="images/cta.svg" alt="cta" width={362} height={232} />
-            <button className="btn-primary">
+            <Image src="/images/cta.svg" alt="cta" width={362} height={232} />
+            <Link href="/companions/new" className="btn-primary">
                 <Image src="/icons/plus.svg" alt="plus" width={12} height={12} />
-                <Link href="/companions/new">
-                    <p>Build a New Companion</p>
-                </Link>
-            </button>
+                <p>Build a New Companion</p>
+            </Link>
         </section>
     )
 }
